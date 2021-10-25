05fa53ff3c74dce329baa6da9a25217fa9164479
SOunit <workbookwork7@gmail.com>
2021-10-25 14:31:14 -0700

    change folder

--- 13_deploy/backend/app.js
+++ 13_deploy/backend/app.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 
 app.use((error, req, res, next) => {
   if (req.file) {
-    fs.unlink(req.file.path, err => {
+    fs.unlink(req.file.path, (err) => {
       console.log(err);
     });
   }
@@ -54,6 +54,6 @@ mongoose
   .then(() => {
     app.listen(5000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
