027fa6bd032f677e944bac64f4fd66c46182f309
SOunit <workbookwork7@gmail.com>
2022-01-19 09:04:58 -0800

    add google map

--- 05_react_app/frontend/src/places/pages/UserPlaces.js
+++ 05_react_app/frontend/src/places/pages/UserPlaces.js
@@ -10,7 +10,7 @@ const DUMMY_PLACES = [
     imageUrl:
       "https://images.unsplash.com/photo-1642414200199-42728a33bf12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
     address: "350 W Georgia St, Vancouver, BC",
-    coodinates: { lat: "49.2786062", lng: "-123.0999113" },
+    location: { lat: 49.2786062, lng: -123.0999113 },
     creator: "u1",
   },
   {
@@ -20,7 +20,7 @@ const DUMMY_PLACES = [
     imageUrl:
       "https://images.unsplash.com/photo-1642414200199-42728a33bf12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
     address: "350 W Georgia St, Vancouver, BC",
-    coodinates: { lat: "49.2786062", lng: "-123.0999113" },
+    coodinates: { lat: 49.2786062, lng: -123.0999113 },
     creator: "u2",
   },
 ];
