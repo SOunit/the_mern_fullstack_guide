ae8f984cf3b39ba8a61c3cc6cbc385f82105b2a1
SOunit <workbookwork7@gmail.com>
2022-01-29 14:19:02 -0800

    add new project

---
+++ 07_node_app/frontend/src/places/pages/NewPlace.js
@@ -0,0 +1,70 @@
+import React from "react";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
+import "./PlaceForm.css";
+
+const NewPlace = () => {
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+      address: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const placeSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  return (
+    <form className="place-form" onSubmit={placeSubmitHandler}>
+      <Input
+        id="title"
+        type="text"
+        label="Title"
+        element="input"
+        validators={[VALIDATOR_REQUIRE()]}
+        onInput={inputHandler}
+        errorText={"Please input valid title."}
+      />
+      <Input
+        id="description"
+        label="Description"
+        element="textarea"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText={"Please input valid description(at least 5 characters)."}
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        type="text"
+        label="Address"
+        element="input"
+        validators={[VALIDATOR_REQUIRE()]}
+        onInput={inputHandler}
+        errorText={"Please input valid address."}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
+    </form>
+  );
+};
+
+export default NewPlace;
