591e93230e30062d48d3d83e929f89a7dbe3c3e0
SOunit <workbookwork7@gmail.com>
2022-01-30 16:55:08 -0800

    add fetching location

--- 07_node_app/backend/controllers/places-controller.js
+++ 07_node_app/backend/controllers/places-controller.js
@@ -1,6 +1,7 @@
 const HttpError = require("../models/http-error");
 const { v4: uuid } = require("uuid");
 const { validationResult } = require("express-validator");
+const getCoordsForAddress = require("../util/location");
 
 let DUMMY_PLACES = [
   {
@@ -53,18 +54,29 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    // not throw, next when using async code
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
   }
 
-  const { title, description, coodinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress();
+  } catch (error) {
+    return next(error);
+  }
+
   const createdPlace = {
     id: uuid(),
     title,
     description,
-    location: coodinates,
+    location: coordinates,
     address,
     creator,
   };
