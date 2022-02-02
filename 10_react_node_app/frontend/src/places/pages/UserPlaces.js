b32999fcd8a4267f9fd26731d26e04ad0ae0f7a4
SOunit <workbookwork7@gmail.com>
2022-02-02 15:14:57 -0800

    add fetching places

--- 10_react_node_app/frontend/src/places/pages/UserPlaces.js
+++ 10_react_node_app/frontend/src/places/pages/UserPlaces.js
@@ -1,35 +1,41 @@
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import PlaceList from "../components/PlaceList";
 import { useParams } from "react-router-dom";
-
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "title1",
-    description: "description1",
-    imageUrl:
-      "https://images.unsplash.com/photo-1642414200199-42728a33bf12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-    address: "350 W Georgia St, Vancouver, BC",
-    location: { lat: 49.2786062, lng: -123.0999113 },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "title2",
-    description: "description2",
-    imageUrl:
-      "https://images.unsplash.com/photo-1642414200199-42728a33bf12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-    address: "350 W Georgia St, Vancouver, BC",
-    coodinates: { lat: 49.2786062, lng: -123.0999113 },
-    creator: "u2",
-  },
-];
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const UserPlaces = () => {
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
 
-  return <PlaceList items={loadedPlaces} />;
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/user/${userId}`
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchPlaces();
+  }, [sendRequest, userId]);
+
+  return (
+    <Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </Fragment>
+  );
 };
 
 export default UserPlaces;
