e45d2582dec17ce727860c79fb12d4fc51e57051
SOunit <workbookwork7@gmail.com>
2022-02-03 07:35:10 -0800

    add new project

---
+++ 11_image_upload/frontend/src/places/pages/NewPlace.js
@@ -0,0 +1,99 @@
+import React, { Fragment, useContext } from "react";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
+import "./PlaceForm.css";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import { AuthContext } from "../../shared/context/auth-context";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHistory } from "react-router-dom";
+
+const NewPlace = () => {
+  const auth = useContext(AuthContext);
+  const history = useHistory();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+      address: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const placeSubmitHandler = async (event) => {
+    console.log("submit");
+    event.preventDefault();
+    try {
+      await sendRequest(
+        "http://localhost:5000/api/places",
+        "POST",
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+          address: formState.inputs.address.value,
+          creator: auth.userId,
+        }),
+        { "Content-Type": "application/json" }
+      );
+
+      history.push("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      <form className="place-form" onSubmit={placeSubmitHandler}>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <Input
+          id="title"
+          type="text"
+          label="Title"
+          element="input"
+          validators={[VALIDATOR_REQUIRE()]}
+          onInput={inputHandler}
+          errorText={"Please input valid title."}
+        />
+        <Input
+          id="description"
+          label="Description"
+          element="textarea"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText={"Please input valid description(at least 5 characters)."}
+          onInput={inputHandler}
+        />
+        <Input
+          id="address"
+          type="text"
+          label="Address"
+          element="input"
+          validators={[VALIDATOR_REQUIRE()]}
+          onInput={inputHandler}
+          errorText={"Please input valid address."}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          ADD PLACE
+        </Button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default NewPlace;
