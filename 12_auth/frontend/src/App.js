8f6e4727122018f8e3ed70d4a24733e38f3c5d15
SOunit <workbookwork7@gmail.com>
2022-02-04 18:42:49 -0800

    add token to local strage

--- 12_auth/frontend/src/App.js
+++ 12_auth/frontend/src/App.js
@@ -19,6 +19,7 @@ const App = () => {
 
   const login = useCallback((uid, token) => {
     setToken(token);
+    localStorage.setItem("userData", JSON.stringify({ userId: uid, token }));
     setUserId(uid);
   }, []);
 
